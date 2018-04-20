5eb9e8659e231aeb7dc10ab07f8e7787e49968fc
DanRamage <ChiefDan@gmail.com>
2018-04-20 13:38:17 -0400

    Initial checkin.

--- scripts/get_historical_nexrad_data.sh
+++ scripts/get_historical_nexrad_data.sh
@@ -2,4 +2,4 @@
 
 source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
 
-python /home/xeniaprod/scripts/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/KillDevilHillsWQ/config/kdh_historical_build_config.ini --ImportData=
+python /home/xeniaprod/scripts/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/KillDevilHillsWQ/config/kdh_historical_build_config.ini --ImportData=/mnt/xmrg_data/2005
