01c5401039c958a9e43de3a20d36b4f21a0b26b2
DanRamage <ChiefDan@gmail.com>
2018-06-22 12:13:33 -0400

    Initial checkin.

--- scripts/nc_sample_data.py
+++ scripts/nc_sample_data.py
@@ -9,21 +9,24 @@ class nc_wq_sample_data(wq_sample_data):
     self._site_id = kwargs.get('site_id', None)
     self._entero_gm = kwargs.get('entero_gm', None)
     self._entero_ssm = kwargs.get('entero_ssm', None)
+
   @property
   def site_id(self):
     return self._site_id
   @site_id.setter
-  def site_id(self, site_id):
-    self._site_id = site_id
+  def site_id(self, value):
+    self._site_id = value
+
   @property
   def entero_gm(self):
     return self._entero_gm
   @entero_gm.setter
-  def entero_gm(self, entero_gm):
-    self._entero_gm = entero_gm
+  def entero_gm(self, value):
+    self._entero_gm = value
+
   @property
   def entero_ssm(self):
     return self._entero_ssm
   @entero_ssm.setter
-  def entero_ssm(self, entero_ssm):
-    self._entero_ssm = entero_ssm
+  def entero_ssm(self, value):
+    self._entero_ssm = value
